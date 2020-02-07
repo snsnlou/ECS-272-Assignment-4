5e126aae3caa794be7cc35cf3076f2ef906fa9df
snsnlou <15103097d@connect.polyu.hk>
2020-02-06 16:44:04 -0800

    Added Pie Chart and Word Cloud (by Trevor Hu)

--- stream.js
+++ stream.js
@@ -181,3 +181,220 @@ function initStreamGraph() {
     streamGraphSVG.append('g')
         .call(yAxisStreamGraph)
 }
+
+
+
+
+var overview1 = [];
+var overview2 = [];
+var overview3 = [];
+var overview4 = [];
+var overview5 = [];
+var overview6 = [];
+
+
+//second and third vis start from here
+
+var tooltip = d3.select("body")
+    .append("div")
+    .attr("class", "tooltip");
+
+
+
+var svg2 = d3.select("body").append("svg")
+    .attr('width', 700)
+    .attr('height', 500)
+    .attr("class", "svg2")
+    
+var svg3 = d3.select("body").append("svg")
+    .attr('width', 700)
+    .attr('height', 500)
+    .attr("class", "svg3")
+
+var width = 700;
+var height = 500;
+
+var r = Math.min(700, 500) / 2.5,
+    color = d3.scaleOrdinal(d3.schemeCategory10),
+    wordColor = d3.scaleOrdinal(d3.schemeCategory20),
+    topics = [], nouns = [], dates =[], people = [], verbs = [], acronyms = [],
+    terms,
+    text,
+    layout,
+    
+    piedata = [{"id":"Topics", "number":0}, 
+            {"id":"Nouns", "number":0}, 
+            {"id":"Dates", "number":0},
+            {"id":"People", "number":0},
+            {"id":"Verbs", "number":0},
+            {"id":"Acronyms", "number":0}];
+
+// d3.csv('./datasets/tmdb\_5000\_movies.csv').then(function(d) {
+//     var data = [];
+//     d.forEach(function(d){
+//         if(d.MetadataDateSent !== "" && d.ExtractedReleaseInPartOrFull !== "" && d.ExtractedBodyText !== ""){
+//             d.MetadataDateSent = d3.timeParse("%Y-%m-%dT%H:00:00+00:00")(d.MetadataDateSent);
+//             data.push(d);
+//         }
+//     });
+//             // create a pie chart
+//         // text = "";
+//         // d.forEach(function(d){
+//         //  text = text + d.ExtractedBodyText + " ";
+//         // });
+//         updatePie("I am right now having problem with it 100 yes 8u283y8 ni 78 882 2 ok yes incredible");
+// });
+d3.csv("./datasets/tmdb\_5000\_movies.csv").then(function(d) {
+    d.forEach(function(d){
+        if(d['vote\_average']== 0){
+            overview1.push(d['overview']);
+        }
+        else if(d['vote\_average']<=2 ){
+            overview2.push(d['overview']);
+        }
+        else if(d['vote\_average']<=4 ){
+            overview3.push(d['overview']);
+        }
+        else if(d['vote\_average']<=6 ){
+            overview4.push(d['overview']);
+        }
+        else if(d['vote\_average']<=8 ){
+            overview5.push(d['overview']);
+        }
+        else if(d['vote\_average']<=10 ){
+            overview6.push(d['overview']);
+        }
+    });
+
+    // create a pie chart
+    updatePie(overview1.join());
+});
+
+
+// create and update the pie chart
+function updatePie(text){
+    svg2.selectAll("*").remove();
+    svg3.selectAll("*").remove();
+    // add a text instruction
+    svg2.append("g")
+        .append("text")
+        .text(function(d){return "Select one term to reveal the word cloud below"})
+        .attr("x", width / 2)
+        .attr("y", 15)
+        .attr("text-anchor", "middle")
+        .attr("font-family", "sans-serif")
+        .attr("font-size", "16px")
+        .attr("fill", "black");
+    
+    // NLP
+    if(text.length > 30000){
+        text = text.substring(0, 30000);
+    }
+    topics = nlp(text).topics().slice(0, 50).out("frequency");
+    nouns = nlp(text).nouns().slice(0, 50).out("frequency");
+    dates = nlp(text).dates().slice(0, 50).out("frequency");
+    people = nlp(text).people().slice(0, 50).out("frequency");
+    verbs = nlp(text).verbs().slice(0, 50).out("frequency");
+    acronyms = nlp(text).acronyms().slice(0, 50).out("frequency");
+    
+    // set the number of words
+    piedata[0].number = topics.length;
+    piedata[1].number = nouns.length;
+    piedata[2].number = dates.length;
+    piedata[3].number = people.length;
+    piedata[4].number = verbs.length;
+    piedata[5].number = acronyms.length;
+    terms = [topics, nouns, dates, people, verbs, acronyms];
+        
+    var pie = d3.pie()
+        .value(function(d) { return d.number; });
+    
+    var path = d3.arc()
+        .outerRadius(r)
+        .innerRadius(0);
+    
+    var label = d3.arc()
+    .outerRadius(r - 100)
+    .innerRadius(r - 50);
+    
+    var arc = svg2.selectAll(".slice")
+        .data(pie(piedata))
+        .enter()
+        .append("g")
+        .attr("class", "slice")
+        .attr("transform", "translate(" + width / 2 + "," + (height / 2 + 15) + ")")
+        .style("cursor", "pointer");
+
+    arc.append("path")
+        .attr("d", path)
+        .attr("stroke", "blue")
+        .attr("fill", function(d, i) { return color(i); })
+        .attr("style", "fill-opacity:0.85;")
+
+    arc.append("text")
+        .attr("transform", function(d) {
+            return "translate(" + label.centroid(d) + ")";
+        })
+        .attr("text-anchor", "middle")
+        .text(function(d, i) { return piedata[i].id; });
+    
+    arc.on("mouseover", function(d, i){
+        d3.select(this).select("path").attr("style", "fill-opacity:1;");
+        tooltip.style("visibility", "visible")
+                .html("Number of Words = " + terms[i].length);
+        })
+    
+        .on("mousemove", function() {
+            tooltip.style("top", (event.pageY - 20) + "px")
+                    .style("left", (event.pageX + 5) + "px");
+        })
+  
+        .on("mouseout", function() {
+            d3.select(this).select("path").attr("style", "fill-opacity:0.85;");
+            tooltip.style("visibility", "hidden");
+        })
+        
+        .on("click", function(d, i){
+            svg3.selectAll("*").remove();
+            
+            // set the word cloud layout
+            layout = d3.layout.cloud()
+            .size([700, 400])
+            .words(terms[i].map(function(d) {return {text: d.normal, size: +d.count * 20};}))
+            .padding(2)
+            .font("Impact")
+            .fontSize(function(d) { return d.size; })
+            .on("end", function(d) { draw(d, i) })
+            .start();
+        });
+    
+}
+
+
+//third vis
+
+function draw(words, i) {
+    svg3.append("g")
+        .append("text")
+        .text(function(d){return "This is the word cloud of the " +" \"" + piedata[i].id + "\""+ " in the movie overview."})
+        .attr("x", width / 2)
+        .attr("y", 30)
+        .attr("text-anchor", "middle")
+        .attr("font-family", "sans-serif")
+        .attr("font-size", "16px")
+        .attr("fill", "black");
+    
+    svg3.append("g")
+        .attr("transform", "translate(" + 350 + "," + 250 + ")")
+        .selectAll("text1")
+        .data(words)
+        .enter().append("text")
+        .style("font-size", function(d) { return d.size + "px"; })
+        .style("font-family", "Impact")
+        .attr("text-anchor", "middle")
+        .attr("transform", function(d) {
+            return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+        })
+        .style("fill", function(d, i) { return color(i); })
+        .text(function(d) { return d.text; });
+}
